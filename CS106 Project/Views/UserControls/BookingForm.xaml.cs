518d45e7f73837f092c6a45e9f9c195c111a880d
JasonnnW056 <williamjason056@gmail.com>
2025-05-30 16:17:35 +1200

    Added booking form

---
+++ CS106 Project/Views/UserControls/BookingForm.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CS106_Project.Views.UserControls
+{
+    /// <summary>
+    /// Interaction logic for BookingForm.xaml
+    /// </summary>
+    public partial class BookingForm : UserControl
+    {
+        public BookingForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
